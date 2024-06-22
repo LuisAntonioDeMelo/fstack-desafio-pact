b224a01e1e6364fc00afd4a443a0909150233ca2
luis antonio <tyluis20@gmail.com>
2024-06-21 21:01:00 -0300

    fix

--- vagas-backend/vagas/src/main/java/com/vagas/app/infra/repository/CandidatoRepository.java
+++ vagas-backend/vagas/src/main/java/com/vagas/app/infra/repository/CandidatoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CandidatoRepository extends JpaRepository<Candidato, UUIDs> {
+public interface CandidatoRepository extends JpaRepository<Candidato, UUID> {
 }
